c50587955753e9ca6be6e4aa9fb70bbaabdaf957
Andreas Kobal <kobalandrey@gmail.com>
2023-02-24 04:07:23 +0000

    finish

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post('/', async (req, res) => {
 });
 
 app.get('/result', (req, res) => {
-  if (global.result.lenght > 0) {
+  if (global.result.length > 0) {
     res.send(global.result);
   } else {
     res.status(404).send('Result not ready');
