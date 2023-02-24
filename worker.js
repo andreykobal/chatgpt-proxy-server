83fc4f8c6c5bdc5f656a6a907b8c6a0ef508f865
Andreas Kobal <kobalandrey@gmail.com>
2023-02-24 03:15:00 +0000

    The modified code starts the worker as soon as the prompt is accepted, and sends a request accepted response to the client without waiting for the worker to complete. The client can fetch the result by sending a

--- worker.js
+++ worker.js
@@ -1,39 +1,38 @@
 const { parentPort } = require('worker_threads');
 
 const startChat = async (inputPrompt, accessToken) => {
-const { ChatGPTUnofficialProxyAPI } = await import('chatgpt');
-const ora = await import('ora');
-const api = new ChatGPTUnofficialProxyAPI({
-accessToken,
-debug: false
-});
+  const { ChatGPTUnofficialProxyAPI } = await import('chatgpt');
+  const ora = await import('ora');
+  const api = new ChatGPTUnofficialProxyAPI({
+    accessToken,
+    debug: false
+  });
 
-let prompt = inputPrompt;
+  let prompt = inputPrompt;
 
-const res1 = await ora.oraPromise(api.sendMessage(prompt), {
-text: prompt
-});
+  const res1 = await ora.oraPromise(api.sendMessage(prompt), {
+    text: prompt
+  });
 
-const prompt2 = 'Finish';
+  const prompt2 = 'Finish';
 
-const res2 = await ora.oraPromise(
-api.sendMessage(prompt2, {
-conversationId: res1.conversationId,
-parentMessageId: res1.id
-}),
-{
-text: prompt2
-}
-);
+  const res2 = await ora.oraPromise(
+    api.sendMessage(prompt2, {
+      conversationId: res1.conversationId,
+      parentMessageId: res1.id
+    }),
+    {
+      text: prompt2
+    }
+  );
 
-const result = res1.text + "\n" + res2.text;
-global.result = result;
+  const result = res1.text + "\n" + res2.text;
+  global.result = result;
 };
 
 if (parentPort) {
-parentPort.on('message', async ({ inputPrompt, accessToken }) => {
-const response = await startChat(inputPrompt, accessToken);
-parentPort.postMessage(response);
-});
+  parentPort.on('message', async ({ inputPrompt, accessToken }) => {
+    const response = await startChat(inputPrompt, accessToken);
+    parentPort.postMessage(response);
+  });
 }
-
